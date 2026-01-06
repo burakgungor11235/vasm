d9b38eddf103ad72d9baf31fe06646ccb5059f3c
burak gungor <burakgungor11235@gmail.com>
2026-01-06 23:35:06 +0300

    feat: implement all ARM-like VM instructions
    - Add MOV, MVN, ADD, ADC, SUB, SBC, RSB, RSC
    - Add AND, EOR, ORR, BIC logical operations
    - Add CMP, CMN, TST, TEQ comparison operations
    - Add MUL, MLA multiply instructions
    - Add LDR, LDRB, STR, STRB memory operations
    - Add B, BL, BX branch instructions
    - Add HALT, SWI, NOP system instructions
    - Implement operand2 decoder for immediates and register shifts

--- src/vm/core.c
+++ src/vm/core.c
@@ -3,48 +3,34 @@
 #include <string.h>
 #include "../../include/vm.h"
 
-static void exec_mov(vm_state_t *vm, u32 instr) {
-  u8 rd = (instr >> 12) & 0xF;
-  u32 operand = 0;
-  (void)operand;
-  vm_set_reg(vm, rd, 0);
-  set_nzcv(vm, 0);
-}
-
-static void exec_add(vm_state_t *vm, u32 instr) {
-  (void)vm;
-  (void)instr;
-}
-
-static void exec_sub(vm_state_t *vm, u32 instr) {
-  (void)vm;
-  (void)instr;
-}
-
-static void exec_and(vm_state_t *vm, u32 instr) {
-  (void)vm;
-  (void)instr;
-}
-
-static void exec_orr(vm_state_t *vm, u32 instr) {
-  (void)vm;
-  (void)instr;
-}
-
-static void exec_b(vm_state_t *vm, u32 instr) {
-  (void)vm;
-  (void)instr;
-}
-
-static void exec_bl(vm_state_t *vm, u32 instr) {
-  (void)vm;
-  (void)instr;
-}
-
-static void exec_halt(vm_state_t *vm, u32 instr) {
-  (void)instr;
-  vm->running = 0;
-}
+extern void exec_mov(vm_state_t *vm, u32 instr);
+extern void exec_mvn(vm_state_t *vm, u32 instr);
+extern void exec_add(vm_state_t *vm, u32 instr);
+extern void exec_adc(vm_state_t *vm, u32 instr);
+extern void exec_sub(vm_state_t *vm, u32 instr);
+extern void exec_sbc(vm_state_t *vm, u32 instr);
+extern void exec_rsb(vm_state_t *vm, u32 instr);
+extern void exec_rsc(vm_state_t *vm, u32 instr);
+extern void exec_and(vm_state_t *vm, u32 instr);
+extern void exec_eor(vm_state_t *vm, u32 instr);
+extern void exec_orr(vm_state_t *vm, u32 instr);
+extern void exec_bic(vm_state_t *vm, u32 instr);
+extern void exec_cmp(vm_state_t *vm, u32 instr);
+extern void exec_cmn(vm_state_t *vm, u32 instr);
+extern void exec_tst(vm_state_t *vm, u32 instr);
+extern void exec_teq(vm_state_t *vm, u32 instr);
+extern void exec_mul(vm_state_t *vm, u32 instr);
+extern void exec_mla(vm_state_t *vm, u32 instr);
+extern void exec_ldr(vm_state_t *vm, u32 instr);
+extern void exec_ldrb(vm_state_t *vm, u32 instr);
+extern void exec_str(vm_state_t *vm, u32 instr);
+extern void exec_strb(vm_state_t *vm, u32 instr);
+extern void exec_b(vm_state_t *vm, u32 instr);
+extern void exec_bl(vm_state_t *vm, u32 instr);
+extern void exec_bx(vm_state_t *vm, u32 instr);
+extern void exec_halt(vm_state_t *vm, u32 instr);
+extern void exec_swi(vm_state_t *vm, u32 instr);
+extern void exec_nop(vm_state_t *vm, u32 instr);
 
 typedef void (*exec_fn)(vm_state_t *, u32);
 
@@ -52,13 +38,33 @@ static exec_fn exec_table[256] = {0};
 
 static void init_exec_table(void) {
   exec_table[OP_MOV] = exec_mov;
+  exec_table[OP_MVN] = exec_mvn;
   exec_table[OP_ADD] = exec_add;
+  exec_table[OP_ADC] = exec_adc;
   exec_table[OP_SUB] = exec_sub;
+  exec_table[OP_SBC] = exec_sbc;
+  exec_table[OP_RSB] = exec_rsb;
+  exec_table[OP_RSC] = exec_rsc;
   exec_table[OP_AND] = exec_and;
+  exec_table[OP_EOR] = exec_eor;
   exec_table[OP_ORR] = exec_orr;
+  exec_table[OP_BIC] = exec_bic;
+  exec_table[OP_CMP] = exec_cmp;
+  exec_table[OP_CMN] = exec_cmn;
+  exec_table[OP_TST] = exec_tst;
+  exec_table[OP_TEQ] = exec_teq;
+  exec_table[OP_MUL] = exec_mul;
+  exec_table[OP_MLA] = exec_mla;
+  exec_table[OP_LDR] = exec_ldr;
+  exec_table[OP_LDRB] = exec_ldrb;
+  exec_table[OP_STR] = exec_str;
+  exec_table[OP_STRB] = exec_strb;
   exec_table[OP_B] = exec_b;
   exec_table[OP_BL] = exec_bl;
+  exec_table[OP_BX] = exec_bx;
   exec_table[OP_HALT] = exec_halt;
+  exec_table[OP_SWI] = exec_swi;
+  exec_table[OP_NOP] = exec_nop;
 }
 
 void vm_step(vm_state_t *vm) {
